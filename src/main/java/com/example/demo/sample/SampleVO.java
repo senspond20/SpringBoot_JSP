b8240d4cac8bc85ec40f8af393decb966334da80
senspond20 <senspond@gmail.com>
2020-11-14 19:19:52 +0900

    lombok Test

---
+++ src/main/java/com/example/demo/sample/SampleVO.java
@@ -0,0 +1,18 @@
+package com.example.demo.sample;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+//@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class SampleVO {
+	private String id;
+	private String name;
+}
